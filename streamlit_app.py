1f45c65434adef738e69a231a384746275b91ea6
robkrouse <39604526+robkrouse@users.noreply.github.com>
2022-09-09 11:50:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥣 wow')
 streamlit.text('🐔look what i did')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
